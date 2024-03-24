7af4fdb2c4787b8d313cd33b037eb4ecca2a38ff
Cyan <joresserwe12@gmail.com>
2024-03-25 02:23:32 +0900

    Page 라우팅

---
+++ src/app/products/page.tsx
@@ -0,0 +1,3 @@
+export default function ProductPage() {
+  return <h2>상품 소개 페이지</h2>;
+}
---
+++ src/app/products/pants/page.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function PantsPage() {
+  return <div>바지 제품 설명</div>;
+}
