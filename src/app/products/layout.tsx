2ac089e2884ab8a6848bb46b3269e583f1642691
Cyan <joresserwe12@gmail.com>
2024-03-30 03:13:33 +0900

    Layout

---
+++ src/app/products/layout.tsx
@@ -0,0 +1,13 @@
+import styles from './layout.module.css';
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <nav className={styles.nav}>
+        <a href="">여성 옷</a>
+        <a href="">남성 옷</a>
+      </nav>
+      <section className={styles.product}>{children}</section>
+    </>
+  );
+}
