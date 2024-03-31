6e6baa92dd9c00009dfb1bd09039e3bf8b407deb
Cyan <joresserwe12@gmail.com>
2024-04-01 01:50:46 +0900

    Server Component/Client Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import os from 'os';
+
+import Counter from '@/components/Counter';
+
 export default function Home() {
-  return <h1>홈페이지다</h1>;
+  console.log('안녕 - 서버');
+  console.log(os.hostname());
+  return (
+    <>
+      <h1>홈페이지다</h1>
+      <Counter></Counter>
+    </>
+  );
 }
